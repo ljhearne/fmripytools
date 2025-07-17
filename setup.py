018fb91c9b5619d3616ea5b1f7a7156785b6f225
ljhearne <luke.hearne@qimrberghofer.edu.au>
2025-07-17 15:42:14 +1000

    troubleshoot pkg

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='fmripytools',
+    version='0.1',
+    description='Python based fMRI tools',
+    author='LJH',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'nilearn',
+        'nibabel',
+        'numpy',
+        'scipy',
+    ],
+    entry_points={
+        'console_scripts': [
+            'denoise = fmripytools.denoise:main',
+        ],
+    }
+)
